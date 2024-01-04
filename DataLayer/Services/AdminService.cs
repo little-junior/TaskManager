86b59a9509ddcce4beb93b9216cbc8b8377a6160
little-junior <rampagedolol@outlook.com>
2024-01-04 16:01:47 -0300

    feat: add AdminService and interface

---
+++ DataLayer/Services/AdminService.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.DataAccess.Interfaces;
+using DataLayer.Services.Interfaces;
+using ModelLayer.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly IDataManagement<TechLeader>? _techLeaderDataManagement;
+
+        public AdminService(IDataManagement<TechLeader>? techLeaderDataManagement)
+        {
+            _techLeaderDataManagement = techLeaderDataManagement;
+        }
+
+        public void AddTechLeader(TechLeader techLeader)
+        {
+            _techLeaderDataManagement.WriteJSON(techLeader);
+        }
+    }
+}
---
+++ DataLayer/Services/Interfaces/IAdminService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer.Users;
+
+namespace DataLayer.Services.Interfaces
+{
+    internal interface IAdminService
+    {
+        void AddTechLeader(TechLeader techLeader);
+    }
+}
