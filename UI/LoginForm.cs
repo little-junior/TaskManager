6073e2aab4d145b463f7f0ed30042ceb8f40d415
little-junior <rampagedolol@outlook.com>
2024-01-04 16:07:05 -0300

    feat: add Admin authentication and form execution

--- UI/LoginForm.cs
+++ UI/LoginForm.cs
@@ -10,20 +10,15 @@ using System.Windows.Forms;
 using DataLayer.Services.Interfaces;
 using DataLayer.DataAccess;
 using UI.Developer;
+using UI.AdminFolder;
 
 namespace UI
 {
-    //public enum UserType
-    //{
-    //    Developer,
-    //    TechLeader
-    //}
-
     public partial class LoginForm : Form
     {
         private readonly IAuthenticationService _authenticationService;
 
-        //private UserType _userType;
+        
 
         public LoginForm(IAuthenticationService authenticationService)
         {
@@ -31,41 +26,37 @@ namespace UI
             _authenticationService = authenticationService;
         }
 
-        //    public LoginForm(IAuthenticationService authenticationService)
-        //    {
-        //        InitializeComponent();
-        //        _authenticationService = authenticationService;
-        //    }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
-
-            //_userType = DetermineUserType(textBox1.Text);
-
-            //OpenCorrespondingForm();
-
-
-
             string keyAccess = textBox1.Text;
 
-            var AutenticatedTechLeader = _authenticationService.TechLeaderAuthenticate(keyAccess);
-            var AutenticatedDeveloper = _authenticationService.DeveloperAuthenticate(keyAccess);
+            var AuthenticatedTechLeader = _authenticationService.TechLeaderAuthenticate(keyAccess);
+            var AuthenticatedDeveloper = _authenticationService.DeveloperAuthenticate(keyAccess);
 
-            bool isTechLeaderAutenticated = AutenticatedTechLeader != null;
-            bool isDeveloperAutenticated = AutenticatedDeveloper != null;
+            bool isTechLeaderAuthenticated = AuthenticatedTechLeader != null;
+            bool isDeveloperAuthenticated = AuthenticatedDeveloper != null;
+            bool isAdminAuthenticated = _authenticationService.AdminAuthenticate(keyAccess);
 
-            if (isTechLeaderAutenticated)
+            if (isTechLeaderAuthenticated)
             {
                 MessageBox.Show("BEM VINDO TECH LEADER");
-                var techLeaderForm = new TechLeaderForm(AutenticatedTechLeader);
+                var techLeaderForm = new TechLeaderForm(AuthenticatedTechLeader);
                 techLeaderForm.Show();
                 this.Hide();
             }
-            else if (isDeveloperAutenticated)
+            else if (isDeveloperAuthenticated)
             {
                 MessageBox.Show("BEM VINDO DEVELOPER");
-                var developerForm = new DeveloperForm(AutenticatedDeveloper);
+                var developerForm = new DeveloperForm(AuthenticatedDeveloper);
                 developerForm.Show();
+                this.Hide();
+            }
+            else if (isAdminAuthenticated)
+            {
+                MessageBox.Show("BEM VINDO ADMIN");
+                var adminForm = new AdminForm();
+                adminForm.Show();
                 this.Hide();
             }
             else
@@ -73,38 +64,7 @@ namespace UI
                 MessageBox.Show("USUARIO NAO ENCONTRADO. TENTE NOVAMENTE");
             }
 
-            //private UserType DetermineUserType(string keyAccess)
-            //{
-            //    // Implemente a lógica para determinar o tipo de usuário com base no keyAccess
-            //    // Pode ser algo simples, como verificar se o keyAccess começa com "tec" para TechLeader
-            //    return keyAccess.StartsWith("tec") ? UserType.TechLeader : UserType.Developer;
-            //}
-
-            //private void OpenCorrespondingForm()
-            //{
-            //    // Abrir o formulário correspondente com base no tipo de usuário
-            //    switch (_userType)
-            //    {
-            //        case UserType.Developer:
-            //            var developerForm = new DeveloperForm();
-            //            developerForm.Show();
-            //            break;
-
-            //        case UserType.TechLeader:
-            //            var techLeaderForm = new TechLeaderForm();
-            //            techLeaderForm.Show();
-            //            break;
-
-            //        // Adicione casos para outros tipos de usuário, se necessário
-
-            //        default:
-            //            break;
-            //    }
-
-            //    // Fechar o formulário de login após abrir o formulário correspondente
-            //    this.Hide();
-            //}
-            //    }
+            
 
         }
     }
