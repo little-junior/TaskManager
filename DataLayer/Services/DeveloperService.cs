bf5bb8a29c81f42213842e128d5a14391431af9f
little-junior <rampagedolol@outlook.com>
2024-01-04 23:01:59 -0300

    feat: add service constructors and gettasksbydeveloper method

--- DataLayer/Services/DeveloperService.cs
+++ DataLayer/Services/DeveloperService.cs
@@ -6,21 +6,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using RealTask = ModelLayer.Task;
 namespace DataLayer.Services
 {
     public class DeveloperService : IDeveloperService
     {
         private readonly IDataManagement<Developer> _developerDataManagement;
+        private readonly IDataManagement<RealTask.Task> _taskDataManagement;
 
         public DeveloperService(IDataManagement<Developer> developerDataManagement)
         {
             _developerDataManagement = developerDataManagement;
         }
 
+        public DeveloperService(IDataManagement<RealTask.Task> taskDataManagement)
+        {
+            _taskDataManagement = taskDataManagement;
+        }
+
         public List<Developer> GetDevelopers()
         {
             return _developerDataManagement.ReadJSON();
+        }
+
+        public List<RealTask.Task> GetTasksByDeveloper(string userId)
+        {
+            return _taskDataManagement.ReadJSON().FindAll(t => t.Responsible.Id == userId);
         }
     }
 }
