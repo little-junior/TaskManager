4dedd2a88038a78cfce992ac4d766ecb454f51eb
little-junior <rampagedolol@outlook.com>
2024-01-07 21:20:25 -0300

    feat: implement all Task Edit Form functionalities

--- UI/TaskFolder/TaskEditForm.cs
+++ UI/TaskFolder/TaskEditForm.cs
@@ -1,6 +1,7 @@
 ﻿using DataLayer.DataAccess;
 using DataLayer.Services;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RealTask = ModelLayer.Task;
-
+using ModelLayer.Enums;
+using ModelLayer.Users;
 namespace UI.TaskFolder
 {
     public partial class TaskEditForm : Form
@@ -21,13 +23,49 @@ namespace UI.TaskFolder
             _taskService = new TaskService(new TaskDataManagement());
             _taskAtual = task;
 
+            _developerService = new DeveloperService(new DeveloperDataManagement());
+            _techLeaderService = new TechLeaderService(new TechLeaderDataManagement());
+
+            ArrayList users = new ArrayList(_developerService.GetDevelopers());
+            users.AddRange(_techLeaderService.GetTechLeaders());
+            PreencherComboBoxResponsaveis(users);
+            PreencherComboBoxStatus();
+
             txtboxNome.Text = task.Name;
             txtboxDescricao.Text = task.Description;
+            numericTempo.Value = task.TaskDaySpan;
+            cbboxStatus.SelectedItem = task.Status.ToString();
+        }
+
+        private void PreencherComboBoxResponsaveis(ArrayList users)
+        {
+            cbboxResponsavel.DisplayMember = "NameSpecializationPosition";
+            cbboxResponsavel.ValueMember = "Id";
+
+            User responsavel = users.Cast<User>().FirstOrDefault(user => user.Id == _taskAtual.Responsible.Id);
+
+            cbboxResponsavel.DataSource = users;
+            cbboxResponsavel.SelectedItem = responsavel;
+        }
+
+        private void PreencherComboBoxStatus()
+        {
+            
+            foreach (Status status in Enum.GetValues(typeof(Status)))
+            {
+                if (status == Status.ASerAprovada || status == Status.EmAnalise || status == Status.Concluida)
+                    continue;
+                cbboxStatus.Items.Add(status.ToString());
+            }
+
+            
 
         }
 
         private RealTask.Task _taskAtual;
         private readonly TaskService _taskService;
+        private readonly DeveloperService _developerService;
+        private readonly TechLeaderService _techLeaderService;
 
         internal void DesativarControles()
         {
@@ -44,8 +82,15 @@ namespace UI.TaskFolder
             var tarefasLista = _taskService.GetTasks();
             string nomeAtualizado = txtboxNome.Text;
             string descricaoAtualizada = txtboxDescricao.Text;
+            var responsavelAtualizado = (User)cbboxResponsavel.SelectedItem;
+            var duracaoAtualizada = (int)numericTempo.Value;
+            var statusAtualizado = Enum.Parse<Status>(cbboxStatus.SelectedItem.ToString());
+
             tarefasLista.First(t => t.Id ==  _taskAtual.Id).UpdateName(nomeAtualizado);
             tarefasLista.First(t => t.Id ==  _taskAtual.Id).UpdateDescription(descricaoAtualizada);
+            tarefasLista.First(t => t.Id == _taskAtual.Id).UpdateStatus(statusAtualizado);
+            tarefasLista.First(t => t.Id == _taskAtual.Id).UpdateResponsible(responsavelAtualizado);
+            tarefasLista.First(t => t.Id == _taskAtual.Id).UpdateDaySpan(duracaoAtualizada);
 
             try
             {
