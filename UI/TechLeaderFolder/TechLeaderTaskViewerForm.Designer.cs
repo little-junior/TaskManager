d1d1e877e7f10086d145d6af93c8def380837874
little-junior <rampagedolol@outlook.com>
2024-01-07 21:14:39 -0300

    feat: add TechLeader task Viewer Form

---
+++ UI/TechLeaderFolder/TechLeaderTaskViewerForm.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace UI.TechLeaderFolder
+{
+    partial class TechLeaderTaskViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEditar = new Button();
+            lblStatusBox = new Label();
+            cbboxStatus = new ComboBox();
+            lblStatus = new Label();
+            lblAprovada = new Label();
+            lblDuracao = new Label();
+            lblResponsavel = new Label();
+            lblDescricao = new Label();
+            lblNome = new Label();
+            lboxTasks = new ListBox();
+            btnAprovar = new Button();
+            btnNegar = new Button();
+            btnMarcarConcluida = new Button();
+            btnDevolver = new Button();
+            SuspendLayout();
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(1018, 529);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(112, 34);
+            btnEditar.TabIndex = 19;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.Visible = false;
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // lblStatusBox
+            // 
+            lblStatusBox.AutoSize = true;
+            lblStatusBox.Location = new Point(12, 33);
+            lblStatusBox.Name = "lblStatusBox";
+            lblStatusBox.Size = new Size(64, 25);
+            lblStatusBox.TabIndex = 18;
+            lblStatusBox.Text = "Status:";
+            // 
+            // cbboxStatus
+            // 
+            cbboxStatus.FormattingEnabled = true;
+            cbboxStatus.Location = new Point(240, 30);
+            cbboxStatus.Name = "cbboxStatus";
+            cbboxStatus.Size = new Size(230, 33);
+            cbboxStatus.TabIndex = 17;
+            cbboxStatus.SelectedIndexChanged += cbboxStatus_SelectedIndexChanged;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(506, 487);
+            lblStatus.Margin = new Padding(4, 0, 4, 0);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(69, 25);
+            lblStatus.TabIndex = 16;
+            lblStatus.Text = "Status: ";
+            lblStatus.Visible = false;
+            // 
+            // lblAprovada
+            // 
+            lblAprovada.AutoSize = true;
+            lblAprovada.Location = new Point(502, 390);
+            lblAprovada.Margin = new Padding(4, 0, 4, 0);
+            lblAprovada.Name = "lblAprovada";
+            lblAprovada.Size = new Size(103, 25);
+            lblAprovada.TabIndex = 15;
+            lblAprovada.Text = "Aprovada? ";
+            lblAprovada.Visible = false;
+            // 
+            // lblDuracao
+            // 
+            lblDuracao.AutoSize = true;
+            lblDuracao.Location = new Point(505, 337);
+            lblDuracao.Margin = new Padding(4, 0, 4, 0);
+            lblDuracao.Name = "lblDuracao";
+            lblDuracao.Size = new Size(139, 25);
+            lblDuracao.TabIndex = 14;
+            lblDuracao.Text = "Duração (dias) : ";
+            lblDuracao.Visible = false;
+            // 
+            // lblResponsavel
+            // 
+            lblResponsavel.AutoSize = true;
+            lblResponsavel.Location = new Point(505, 280);
+            lblResponsavel.Margin = new Padding(4, 0, 4, 0);
+            lblResponsavel.Name = "lblResponsavel";
+            lblResponsavel.Size = new Size(119, 25);
+            lblResponsavel.TabIndex = 13;
+            lblResponsavel.Text = "Responsável: ";
+            lblResponsavel.Visible = false;
+            // 
+            // lblDescricao
+            // 
+            lblDescricao.Location = new Point(505, 153);
+            lblDescricao.Margin = new Padding(4, 0, 4, 0);
+            lblDescricao.Name = "lblDescricao";
+            lblDescricao.Size = new Size(607, 105);
+            lblDescricao.TabIndex = 12;
+            lblDescricao.Text = "Descrição: ";
+            lblDescricao.Visible = false;
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(505, 92);
+            lblNome.Margin = new Padding(4, 0, 4, 0);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(70, 25);
+            lblNome.TabIndex = 11;
+            lblNome.Text = "Nome: ";
+            lblNome.Visible = false;
+            // 
+            // lboxTasks
+            // 
+            lboxTasks.FormattingEnabled = true;
+            lboxTasks.ItemHeight = 25;
+            lboxTasks.Location = new Point(12, 92);
+            lboxTasks.Margin = new Padding(4, 5, 4, 5);
+            lboxTasks.Name = "lboxTasks";
+            lboxTasks.Size = new Size(458, 629);
+            lboxTasks.TabIndex = 10;
+            lboxTasks.SelectedIndexChanged += lboxTasks_SelectedIndexChanged_1;
+            // 
+            // btnAprovar
+            // 
+            btnAprovar.Location = new Point(689, 568);
+            btnAprovar.Name = "btnAprovar";
+            btnAprovar.Size = new Size(222, 34);
+            btnAprovar.TabIndex = 20;
+            btnAprovar.Text = "Aprovar Tarefa";
+            btnAprovar.UseVisualStyleBackColor = true;
+            btnAprovar.Visible = false;
+            btnAprovar.Click += btnAprovar_Click;
+            // 
+            // btnNegar
+            // 
+            btnNegar.AutoSize = true;
+            btnNegar.Location = new Point(689, 632);
+            btnNegar.Name = "btnNegar";
+            btnNegar.Size = new Size(222, 35);
+            btnNegar.TabIndex = 21;
+            btnNegar.Text = "Negar Tarefa (abandonar)";
+            btnNegar.UseVisualStyleBackColor = true;
+            btnNegar.Visible = false;
+            btnNegar.Click += btnNegar_Click;
+            // 
+            // btnMarcarConcluida
+            // 
+            btnMarcarConcluida.Location = new Point(519, 568);
+            btnMarcarConcluida.Name = "btnMarcarConcluida";
+            btnMarcarConcluida.Size = new Size(220, 34);
+            btnMarcarConcluida.TabIndex = 22;
+            btnMarcarConcluida.Text = "Marcar como Concluída";
+            btnMarcarConcluida.UseVisualStyleBackColor = true;
+            btnMarcarConcluida.Visible = false;
+            btnMarcarConcluida.Click += btnMarcarConcluida_Click;
+            // 
+            // btnDevolver
+            // 
+            btnDevolver.AutoSize = true;
+            btnDevolver.Location = new Point(594, 632);
+            btnDevolver.Name = "btnDevolver";
+            btnDevolver.Size = new Size(220, 35);
+            btnDevolver.TabIndex = 23;
+            btnDevolver.Text = "Devolver ao Responsável";
+            btnDevolver.UseVisualStyleBackColor = true;
+            btnDevolver.Visible = false;
+            btnDevolver.Click += btnDevolver_Click;
+            // 
+            // TechLeaderTaskViewerForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1143, 750);
+            Controls.Add(btnDevolver);
+            Controls.Add(btnMarcarConcluida);
+            Controls.Add(btnNegar);
+            Controls.Add(btnAprovar);
+            Controls.Add(btnEditar);
+            Controls.Add(lblStatusBox);
+            Controls.Add(cbboxStatus);
+            Controls.Add(lblStatus);
+            Controls.Add(lblAprovada);
+            Controls.Add(lblDuracao);
+            Controls.Add(lblResponsavel);
+            Controls.Add(lblDescricao);
+            Controls.Add(lblNome);
+            Controls.Add(lboxTasks);
+            Name = "TechLeaderTaskViewerForm";
+            Text = "TechLeaderTaskViewerForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEditar;
+        private Label lblStatusBox;
+        private ComboBox cbboxStatus;
+        private Label lblStatus;
+        private Label lblAprovada;
+        private Label lblDuracao;
+        private Label lblResponsavel;
+        private Label lblDescricao;
+        private Label lblNome;
+        private ListBox lboxTasks;
+        private Button btnAprovar;
+        private Button btnNegar;
+        private Button btnMarcarConcluida;
+        private Button btnDevolver;
+    }
+}
---
+++ UI/TechLeaderFolder/TechLeaderTaskViewerForm.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer.DataAccess;
+using DataLayer.Services;
+using ModelLayer.Enums;
+using UI.TaskFolder;
+using RealTask = ModelLayer.Task;
+namespace UI.TechLeaderFolder
+{
+    public partial class TechLeaderTaskViewerForm : Form
+    {
+        public TechLeaderTaskViewerForm()
+        {
+            InitializeComponent();
+
+            _taskService = new TaskService(new TaskDataManagement());
+
+            PopulateComboBox();
+            PopulateListBox();
+        }
+
+        private readonly TaskService _taskService;
+        private RealTask.Task _taskAtual;
+        private string _statusSelecionado;
+
+        private void PopulateComboBox()
+        {
+            cbboxStatus.Items.Add("Todas");
+            foreach (Status status in Enum.GetValues(typeof(Status)))
+            {
+                cbboxStatus.Items.Add(status.ToString());
+            }
+
+            cbboxStatus.SelectedItem = "Todas";
+
+        }
+
+        private void PopulateListBox()
+        {
+            var tasksList = _taskService.GetTasks();
+
+            lboxTasks.DataSource = tasksList;
+            lboxTasks.DisplayMember = "Name";
+        }
+
+        private void PopulateListBox(string option)
+        {
+            var tasksList = _taskService.GetTasks().FindAll(t => t.Status.ToString() == option);
+
+            lboxTasks.DataSource = tasksList;
+            lboxTasks.DisplayMember = "Name";
+        }
+
+
+
+        private void ExibirDetalhesTarefa(RealTask.Task tarefa)
+        {
+            if (tarefa != null)
+            {
+                lblNome.Text = $"Nome: {tarefa.Name}";
+                lblDescricao.Text = $"Descrição: {tarefa.Description}";
+                lblResponsavel.Text = $"Responsável: {tarefa.Responsible.NameSpecializationPosition}";
+                lblDuracao.Text = $"Duração (dias): {tarefa.TaskDaySpan}";
+                lblAprovada.Text = $"Aprovada? -> {tarefa.Approved}";
+                lblStatus.Text = $"Status: {tarefa.Status}";
+
+                ExibirControles();
+            }
+            else
+            {
+                EsconderControles();
+                EsconderOpcoesASerAprovada();
+                EsconderOpcoesEmAnalise();
+                btnEditar.Visible = false;
+            }
+        }
+
+        private void ExibirControles()
+        {
+            lblNome.Visible = true;
+            lblDescricao.Visible = true;
+            lblResponsavel.Visible = true;
+            lblDuracao.Visible = true;
+            lblAprovada.Visible = true;
+            lblStatus.Visible = true;
+        }
+
+        private void EsconderControles()
+        {
+            lblNome.Visible = false;
+            lblDescricao.Visible = false;
+            lblResponsavel.Visible = false;
+            lblDuracao.Visible = false;
+            lblAprovada.Visible = false;
+            lblStatus.Visible = false;
+        }
+
+        private void lboxTasks_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (lboxTasks != null)
+            {
+                RealTask.Task tarefaSelecionada = (RealTask.Task)lboxTasks.SelectedItem;
+                ExibirDetalhesTarefa(tarefaSelecionada);
+                _taskAtual = tarefaSelecionada;
+
+                VerificarStatus();
+            }
+        }
+
+        private void cbboxStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _statusSelecionado = cbboxStatus.SelectedItem.ToString();
+
+            AtualizarListBox();
+        }
+
+        private void AtualizarListBox()
+        {
+            if (_statusSelecionado == "Todas")
+                PopulateListBox();
+            else
+                PopulateListBox(_statusSelecionado);
+        }
+
+        private void AtualizarExibicao()
+        {
+
+            RealTask.Task tarefaSelecionada = (RealTask.Task)lboxTasks.SelectedItem;
+            ExibirDetalhesTarefa(tarefaSelecionada);
+            _taskAtual = tarefaSelecionada;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            var taskEditForm = new TaskEditForm(_taskAtual);
+            taskEditForm.ShowDialog();
+            AtualizarListBox();
+            AtualizarExibicao();
+        }
+
+       
+        
+        private void VerificarStatus()
+        {
+            switch (_taskAtual.Status)
+            {
+                case (Status.EmAnalise):
+                    MostrarOpcoesEmAnalise();
+                    EsconderOpcoesASerAprovada();
+                    btnEditar.Visible = false;
+                    break;
+                case (Status.ASerAprovada):
+                    MostrarOpcoesASerAprovada();
+                    EsconderOpcoesEmAnalise();
+                    btnEditar.Visible = false;
+                    break;
+                case (Status.Abandonada):
+                case (Status.Concluida):
+                    btnEditar.Visible = false;
+                    EsconderOpcoesASerAprovada();
+                    EsconderOpcoesEmAnalise();
+                    break;
+                default:
+                    btnEditar.Visible = true;
+                    EsconderOpcoesASerAprovada();
+                    EsconderOpcoesEmAnalise();
+                    break;
+
+            }
+        }
+
+        private void MostrarOpcoesEmAnalise()
+        {
+            btnMarcarConcluida.Visible = true;
+            btnDevolver.Visible = true;
+
+        }
+
+        private void EsconderOpcoesEmAnalise()
+        {
+            btnMarcarConcluida.Visible = false;
+            btnDevolver.Visible = false;
+        }
+
+        private void MostrarOpcoesASerAprovada()
+        {
+            btnAprovar.Visible = true;
+            btnNegar.Visible = true;
+        }
+
+        private void EsconderOpcoesASerAprovada()
+        {
+            btnAprovar.Visible = false;
+            btnNegar.Visible = false;
+
+        }
+
+        private void btnMarcarConcluida_Click(object sender, EventArgs e)
+        {
+            var tarefasLista = _taskService.GetTasks();
+            tarefasLista.First(t => t.Id == _taskAtual.Id).UpdateStatus(Status.Concluida);
+
+            try
+            {
+                _taskService.UpdateTask(tarefasLista);
+                MessageBox.Show("Tarefa marcada como Concluída", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                AtualizarListBox();
+                AtualizarExibicao();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDevolver_Click(object sender, EventArgs e)
+        {
+            var tarefasLista = _taskService.GetTasks();
+            tarefasLista.First(t => t.Id == _taskAtual.Id).UpdateStatus(Status.EmAndamento);
+
+            try
+            {
+                _taskService.UpdateTask(tarefasLista);
+                MessageBox.Show("Tarefa devolvida para desenvolvimento", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                AtualizarListBox();
+                AtualizarExibicao();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAprovar_Click(object sender, EventArgs e)
+        {
+            var tarefasLista = _taskService.GetTasks();
+            tarefasLista.First(t => t.Id == _taskAtual.Id).SetApproved();
+
+            try
+            {
+                _taskService.UpdateTask(tarefasLista);
+                MessageBox.Show("Tarefa aprovada e mandada para desenvolvimento", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                AtualizarListBox();
+                AtualizarExibicao();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNegar_Click(object sender, EventArgs e)
+        {
+            var tarefasLista = _taskService.GetTasks();
+            tarefasLista.First(t => t.Id == _taskAtual.Id).UpdateStatus(Status.Abandonada);
+
+            try
+            {
+                _taskService.UpdateTask(tarefasLista);
+                MessageBox.Show("Tarefa negada e abandonada", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                AtualizarListBox();
+                AtualizarExibicao();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
