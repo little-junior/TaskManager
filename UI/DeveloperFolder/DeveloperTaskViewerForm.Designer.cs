4d985a1dea749f05b0a5ef175a1dbaef2b30734c
little-junior <rampagedolol@outlook.com>
2024-01-12 10:52:39 -0300

    fix: change controllers visibility

--- UI/DeveloperFolder/DeveloperTaskViewerForm.Designer.cs
+++ UI/DeveloperFolder/DeveloperTaskViewerForm.Designer.cs
@@ -50,11 +50,10 @@
             // 
             lboxTasks.Font = new Font("Segoe UI", 11F);
             lboxTasks.FormattingEnabled = true;
-            lboxTasks.ItemHeight = 30;
-            lboxTasks.Location = new Point(13, 96);
-            lboxTasks.Margin = new Padding(4, 5, 4, 5);
+            lboxTasks.ItemHeight = 20;
+            lboxTasks.Location = new Point(9, 58);
             lboxTasks.Name = "lboxTasks";
-            lboxTasks.Size = new Size(458, 604);
+            lboxTasks.Size = new Size(322, 364);
             lboxTasks.TabIndex = 0;
             lboxTasks.SelectedIndexChanged += lboxTasks_SelectedIndexChanged;
             // 
@@ -62,10 +61,9 @@
             // 
             lblNome.AutoSize = true;
             lblNome.Font = new Font("Segoe UI", 12F);
-            lblNome.Location = new Point(506, 96);
-            lblNome.Margin = new Padding(4, 0, 4, 0);
+            lblNome.Location = new Point(354, 58);
             lblNome.Name = "lblNome";
-            lblNome.Size = new Size(92, 32);
+            lblNome.Size = new Size(60, 21);
             lblNome.TabIndex = 1;
             lblNome.Text = "Nome: ";
             lblNome.Visible = false;
@@ -73,10 +71,9 @@
             // lblDescricao
             // 
             lblDescricao.Font = new Font("Segoe UI", 12F);
-            lblDescricao.Location = new Point(506, 157);
-            lblDescricao.Margin = new Padding(4, 0, 4, 0);
+            lblDescricao.Location = new Point(354, 94);
             lblDescricao.Name = "lblDescricao";
-            lblDescricao.Size = new Size(607, 105);
+            lblDescricao.Size = new Size(425, 63);
             lblDescricao.TabIndex = 2;
             lblDescricao.Text = "Descrição: ";
             lblDescricao.Visible = false;
@@ -85,10 +82,9 @@
             // 
             lblResponsavel.AutoSize = true;
             lblResponsavel.Font = new Font("Segoe UI", 12F);
-            lblResponsavel.Location = new Point(506, 276);
-            lblResponsavel.Margin = new Padding(4, 0, 4, 0);
+            lblResponsavel.Location = new Point(354, 166);
             lblResponsavel.Name = "lblResponsavel";
-            lblResponsavel.Size = new Size(157, 32);
+            lblResponsavel.Size = new Size(104, 21);
             lblResponsavel.TabIndex = 3;
             lblResponsavel.Text = "Responsável: ";
             lblResponsavel.Visible = false;
@@ -97,10 +93,9 @@
             // 
             lblDuracao.AutoSize = true;
             lblDuracao.Font = new Font("Segoe UI", 12F);
-            lblDuracao.Location = new Point(506, 340);
-            lblDuracao.Margin = new Padding(4, 0, 4, 0);
+            lblDuracao.Location = new Point(354, 204);
             lblDuracao.Name = "lblDuracao";
-            lblDuracao.Size = new Size(184, 32);
+            lblDuracao.Size = new Size(121, 21);
             lblDuracao.TabIndex = 4;
             lblDuracao.Text = "Duração (dias) : ";
             lblDuracao.Visible = false;
@@ -109,10 +104,9 @@
             // 
             lblAprovada.AutoSize = true;
             lblAprovada.Font = new Font("Segoe UI", 12F);
-            lblAprovada.Location = new Point(506, 407);
-            lblAprovada.Margin = new Padding(4, 0, 4, 0);
+            lblAprovada.Location = new Point(354, 244);
             lblAprovada.Name = "lblAprovada";
-            lblAprovada.Size = new Size(133, 32);
+            lblAprovada.Size = new Size(88, 21);
             lblAprovada.TabIndex = 5;
             lblAprovada.Text = "Aprovada? ";
             lblAprovada.Visible = false;
@@ -121,10 +115,9 @@
             // 
             lblStatus.AutoSize = true;
             lblStatus.Font = new Font("Segoe UI", 12F);
-            lblStatus.Location = new Point(507, 491);
-            lblStatus.Margin = new Padding(4, 0, 4, 0);
+            lblStatus.Location = new Point(355, 295);
             lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(90, 32);
+            lblStatus.Size = new Size(59, 21);
             lblStatus.TabIndex = 6;
             lblStatus.Text = "Status: ";
             lblStatus.Visible = false;
@@ -133,9 +126,10 @@
             // 
             cbboxStatus.Font = new Font("Segoe UI", 10F);
             cbboxStatus.FormattingEnabled = true;
-            cbboxStatus.Location = new Point(241, 34);
+            cbboxStatus.Location = new Point(169, 20);
+            cbboxStatus.Margin = new Padding(2, 2, 2, 2);
             cbboxStatus.Name = "cbboxStatus";
-            cbboxStatus.Size = new Size(230, 36);
+            cbboxStatus.Size = new Size(162, 25);
             cbboxStatus.TabIndex = 7;
             cbboxStatus.SelectedIndexChanged += cbboxStatus_SelectedIndexChanged;
             // 
@@ -143,18 +137,20 @@
             // 
             lblStatusBox.AutoSize = true;
             lblStatusBox.Font = new Font("Segoe UI", 10F);
-            lblStatusBox.Location = new Point(13, 37);
+            lblStatusBox.Location = new Point(9, 22);
+            lblStatusBox.Margin = new Padding(2, 0, 2, 0);
             lblStatusBox.Name = "lblStatusBox";
-            lblStatusBox.Size = new Size(69, 28);
+            lblStatusBox.Size = new Size(50, 19);
             lblStatusBox.TabIndex = 8;
             lblStatusBox.Text = "Status:";
             // 
             // btnEditar
             // 
             btnEditar.Font = new Font("Segoe UI", 11F);
-            btnEditar.Location = new Point(1013, 533);
+            btnEditar.Location = new Point(709, 320);
+            btnEditar.Margin = new Padding(2, 2, 2, 2);
             btnEditar.Name = "btnEditar";
-            btnEditar.Size = new Size(118, 40);
+            btnEditar.Size = new Size(83, 24);
             btnEditar.TabIndex = 9;
             btnEditar.Text = "Editar";
             btnEditar.UseVisualStyleBackColor = true;
@@ -165,65 +161,75 @@
             // 
             btnMarcarImpedimento.AutoSize = true;
             btnMarcarImpedimento.Font = new Font("Segoe UI", 11F);
-            btnMarcarImpedimento.Location = new Point(665, 630);
+            btnMarcarImpedimento.Location = new Point(466, 378);
+            btnMarcarImpedimento.Margin = new Padding(2, 2, 2, 2);
             btnMarcarImpedimento.Name = "btnMarcarImpedimento";
-            btnMarcarImpedimento.Size = new Size(205, 40);
+            btnMarcarImpedimento.Size = new Size(144, 30);
             btnMarcarImpedimento.TabIndex = 10;
             btnMarcarImpedimento.Text = "Com impedimento";
             btnMarcarImpedimento.UseVisualStyleBackColor = true;
+            btnMarcarImpedimento.Visible = false;
             btnMarcarImpedimento.Click += btnMarcarImpedimento_Click;
             // 
             // lblMarcarComo
             // 
             lblMarcarComo.AutoSize = true;
             lblMarcarComo.Font = new Font("Segoe UI", 11F);
-            lblMarcarComo.Location = new Point(506, 630);
+            lblMarcarComo.Location = new Point(354, 378);
+            lblMarcarComo.Margin = new Padding(2, 0, 2, 0);
             lblMarcarComo.Name = "lblMarcarComo";
-            lblMarcarComo.Size = new Size(153, 30);
+            lblMarcarComo.Size = new Size(104, 20);
             lblMarcarComo.TabIndex = 11;
             lblMarcarComo.Text = "Marcar como: ";
+            lblMarcarComo.Visible = false;
             // 
             // btnMarcarAbandonada
             // 
             btnMarcarAbandonada.AutoSize = true;
             btnMarcarAbandonada.Font = new Font("Segoe UI", 11F);
-            btnMarcarAbandonada.Location = new Point(901, 630);
+            btnMarcarAbandonada.Location = new Point(631, 378);
+            btnMarcarAbandonada.Margin = new Padding(2, 2, 2, 2);
             btnMarcarAbandonada.Name = "btnMarcarAbandonada";
-            btnMarcarAbandonada.Size = new Size(172, 40);
+            btnMarcarAbandonada.Size = new Size(120, 30);
             btnMarcarAbandonada.TabIndex = 12;
             btnMarcarAbandonada.Text = "Abandonada";
             btnMarcarAbandonada.UseVisualStyleBackColor = true;
+            btnMarcarAbandonada.Visible = false;
             btnMarcarAbandonada.Click += btnMarcarAbandonada_Click;
             // 
             // btnMarcarConcluida
             // 
             btnMarcarConcluida.AutoSize = true;
             btnMarcarConcluida.Font = new Font("Segoe UI", 11F);
-            btnMarcarConcluida.Location = new Point(665, 684);
+            btnMarcarConcluida.Location = new Point(466, 410);
+            btnMarcarConcluida.Margin = new Padding(2, 2, 2, 2);
             btnMarcarConcluida.Name = "btnMarcarConcluida";
-            btnMarcarConcluida.Size = new Size(203, 40);
+            btnMarcarConcluida.Size = new Size(145, 30);
             btnMarcarConcluida.TabIndex = 13;
             btnMarcarConcluida.Text = "Concluída (análise)";
             btnMarcarConcluida.UseVisualStyleBackColor = true;
+            btnMarcarConcluida.Visible = false;
             btnMarcarConcluida.Click += btnMarcarConcluida_Click;
             // 
             // btnMarcarAtrasada
             // 
             btnMarcarAtrasada.AutoSize = true;
             btnMarcarAtrasada.Font = new Font("Segoe UI", 11F);
-            btnMarcarAtrasada.Location = new Point(901, 684);
+            btnMarcarAtrasada.Location = new Point(631, 410);
+            btnMarcarAtrasada.Margin = new Padding(2, 2, 2, 2);
             btnMarcarAtrasada.Name = "btnMarcarAtrasada";
-            btnMarcarAtrasada.Size = new Size(172, 40);
+            btnMarcarAtrasada.Size = new Size(120, 30);
             btnMarcarAtrasada.TabIndex = 14;
             btnMarcarAtrasada.Text = "Atrasada";
             btnMarcarAtrasada.UseVisualStyleBackColor = true;
+            btnMarcarAtrasada.Visible = false;
             btnMarcarAtrasada.Click += btnMarcarAtrasada_Click;
             // 
             // DeveloperTaskViewerForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1143, 750);
+            ClientSize = new Size(800, 449);
             Controls.Add(btnMarcarAtrasada);
             Controls.Add(btnMarcarConcluida);
             Controls.Add(btnMarcarAbandonada);
@@ -241,7 +247,6 @@
             Controls.Add(lboxTasks);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(4, 5, 4, 5);
             MaximizeBox = false;
             Name = "DeveloperTaskViewerForm";
             StartPosition = FormStartPosition.CenterScreen;
